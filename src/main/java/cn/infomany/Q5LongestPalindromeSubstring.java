970d371e9ad097c6f465bf47596ef5fb07912ceb
zjbshk <592466695@qq.com>
2020-06-25 10:44:26 +0800

    LeetCode解题代码java，初始化

---
+++ src/main/java/cn/infomany/Q5LongestPalindromeSubstring.java
@@ -0,0 +1,99 @@
+package cn.infomany;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 5. 最长回文子串
+ *
+ * @author zjb
+ * @date 2020/6/25
+ */
+public class Q5LongestPalindromeSubstring {
+    /**
+     * ####题目描述：
+     * >给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+     * <p>
+     * #####示例 1：
+     * <p>
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     * ##### 示例 2：
+     * <p>
+     * 输入: "cbbd"
+     * 输出: "bb"
+     */
+    public String longestPalindrome(String s) {
+
+        int len = s.length();
+        if (len <= 1) {
+            return s;
+        }
+
+        String maxSub = getTheTwoHalvesTheLongestEqual(s, len);
+        if (maxSub.length() >= len - 1) {
+            return maxSub;
+        }
+        Set<String> subList = new HashSet<>();
+        subList.add(maxSub);
+        for (int i = 1; i < len; i++) {
+            String leftPalindrome = getTheTwoHalvesTheLongestEqual(s.substring(0, len - i), len - i);
+            String rightPalindrome = getTheTwoHalvesTheLongestEqual(s.substring(i), len - i);
+            subList.add(leftPalindrome);
+            subList.add(rightPalindrome);
+        }
+        return subList.stream().max(Comparator.comparingInt(String::length)).orElse("");
+    }
+
+    private String getTheTwoHalvesTheLongestEqual(String s, int totalLen) {
+        int oddEvenFlag = totalLen & 1;
+        int centerIndex = totalLen / 2 + oddEvenFlag - 1;
+
+        int halfLong = totalLen / 2;
+        int start = centerIndex, end = centerIndex;
+        for (int i = 1; i <= halfLong; i++) {
+            if (s.charAt(halfLong - i) != s.charAt(centerIndex + i)) {
+                break;
+            }
+            start = halfLong - i;
+            end = centerIndex + i;
+        }
+
+        if (start == end) {
+            return leftRightFindCenterChar(s, centerIndex, totalLen);
+        }
+
+        if (start == 0) {
+            return s;
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private String leftRightFindCenterChar(String s, int centerIndex, int totalLen) {
+        int start = centerIndex;
+        int end = centerIndex;
+        for (int i = centerIndex - 1; i >= 0; i--) {
+            if (s.charAt(i) != s.charAt(centerIndex)) {
+                break;
+            }
+            start--;
+        }
+        if (start != centerIndex) {
+            return s.substring(start, centerIndex + 1);
+        }
+        for (int i = centerIndex + 1; i < totalLen; i++) {
+            if (s.charAt(i) != s.charAt(centerIndex)) {
+                break;
+            }
+            end++;
+        }
+        if (end != centerIndex) {
+            return s.substring(centerIndex, end + 1);
+        }
+        return s.substring(centerIndex, centerIndex + 1);
+    }
+
+}
---
+++ src/main/java/cn/infomany/Q6ZShapedTransformation.java
@@ -0,0 +1,76 @@
+package cn.infomany;
+
+/**
+ * 6. Z 字形变换
+ *
+ * @author zjb
+ * @date 2020/6/25
+ */
+public class Q6ZShapedTransformation {
+
+    /**
+     * ####题目描述：
+     * >将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+     * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+     * <p>
+     * 请你实现这个将字符串进行指定行数变换的函数：
+     * <p>
+     * string convert(string s, int numRows);
+     * <p>
+     * ##### 示例 1:
+     * <p>
+     * 输入: s = "LEETCODEISHIRING", numRows = 3
+     * 输出: "LCIRETOESIIGEDHN"
+     * ##### 示例 2:
+     * <p>
+     * 输入: s = "LEETCODEISHIRING", numRows = 4
+     * 输出: "LDREOEIIECIHNTSG"
+     * 解释:
+     * <p>
+     * L     D     R
+     * E   O E   I I
+     * E C   I H   N
+     * T     S     G
+     */
+
+    public static void main(String[] args) {
+        String leetcodeishiring = new Q6ZShapedTransformation().convert("AB", 1);
+        System.out.println("leetcodeishiring = " + leetcodeishiring);
+    }
+
+    public String convert(String s, int numRows) {
+        int len = s.length();
+        if (len == 0) {
+            return "";
+        }
+        // 一行或者字符串的长度小于行数时直接返回
+        if (numRows == 1 || len <= numRows) {
+            return s;
+        }
+
+        char[] convertChar = new char[len];
+        int k = 0;
+        for (int i = 0; i < numRows; i++) {
+            boolean door = true;
+            for (int j = i; j < len && k < len; ) {
+                convertChar[k++] = s.charAt(j);
+
+                if (i == 0 || i == numRows - 1) {
+                    j += numRows * 2 - 2;
+                    continue;
+                }
+                if (door) {
+                    j += numRows * 2 - 2 - i * 2;
+                } else {
+                    j += 2 * i;
+                }
+                door = !door;
+            }
+        }
+        return new String(convertChar);
+    }
+}
